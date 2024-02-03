4008cb55dc1e5c6bd961f548e96b3425a78e4514
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-04 00:19:39 +0530

    Updated prompt ID0

---
+++ prompt.py
@@ -0,0 +1,11 @@
+class Prompt():
+    def __init__():
+        pass
+
+    def prompt1(ID=0):
+        if ID == 0:
+            prompt_text = """As a YouTube video summarizer, 
+            your task is to take the provided transcript text and generate a concise summary of the entire video, highlighting key points within 250 words. 
+            Please provide an informative and engaging summary of the content, emphasizing important insights and notable aspects discussed in the video. 
+            Use the given text as the basis for your summary : """
+        return prompt_text
