a08a776df7f9fee9429e9435634292bf1c69dd29
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-03 22:46:25 +0530

    streamlit config

--- research/streamlit_demo.py
+++ research/streamlit_demo.py
@@ -5,8 +5,7 @@ st.title("AI Video Summarizer")
 youtube_url = st.text_input("Enter YouTube Video Link")
 
 if youtube_url:
-    video_id = youtube_url.split("v=")[1]
-    st.write(video_id)
+    video_id = youtube_url.split("=")[1]
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get summary"):
