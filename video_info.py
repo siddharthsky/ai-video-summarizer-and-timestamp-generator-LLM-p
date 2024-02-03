efeeb87374b308759b21c85f019e7b63856a03b1
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-03 23:59:57 +0530

    Video url ID retriveal

--- video_info.py
+++ video_info.py
@@ -7,6 +7,10 @@ class GetVideo():
     def __init__(self):
         pass
 
+    def Id(link):
+        video_id = link.split("=")[1]
+        return video_id
+        
     def title(link):
         r = requests.get(link) 
         s = BeautifulSoup(r.text, "html.parser") 
@@ -14,10 +18,15 @@ class GetVideo():
         return title
         
     def transcript(link):
-        video_id = link.split("=")[1]
-        transcript_dict=YouTubeTranscriptApi.get_transcript(video_id)
-        final_transcript = ""
-        for i in transcript_dict:
-            final_transcript += " " + i["text"]
-        return final_transcript
+        video_id=GetVideo.Id(link)
+        
+        try:
+            transcript_dict=YouTubeTranscriptApi.get_transcript(video_id)
+            final_transcript = ""
+            for i in transcript_dict:
+                final_transcript += " " + i["text"]
+            return final_transcript
+    
+        except Exception as e:
+            print(e)
     
