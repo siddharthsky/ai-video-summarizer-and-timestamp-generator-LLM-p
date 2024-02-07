4fffc9a53d52e6f16f83bda4f20d1ed4a9c693e6
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-07 23:48:50 +0530

    Transcript prompt improvement

--- prompt.py
+++ prompt.py
@@ -4,26 +4,32 @@ class Prompt():
 
     def prompt1(ID=0):
         if ID == 0:
-            prompt_text = """As a YouTube video summarizer, 
-            your task is to take the provided transcript text and generate a concise summary of the entire video, highlighting key points within 250 words. 
-            Please provide an informative and engaging summary of the content, emphasizing important insights and notable aspects discussed in the video. 
+            prompt_text = """Your job as an AI video summarizer is to create a short summary of the given video transcript. You need to condense the information into 250 words or less and focus on the important points. Make sure your summary is interesting and informative, highlighting the key insights and notable aspects discussed in the video. Use the provided text as the basis for your summary, and aim to engage the audience by presenting the information in a clear and easy-to-understand way.
             Use the given text as the basis for your summary : """
         
-        elif ID == "0t":
+        elif ID == "transcript":
             prompt_text = """
-            Generate timestamps for the main chapter/topics discussed in a YouTube video transcript.
+            Generate timestamps for main chapter/topics in a YouTube video transcript.
+            Given text segments with their time, generate timestamps for main topics discussed in the video. Format timestamps as hh:mm:ss and provide clear and concise topic titles.  
+           
+            Instructions:
+            1. List only topic titles and timestamps.
+            2. Do not explain the titles.
+            3. Include clickable URLs.
+            4. Provide output in Markdown format.
 
-            Given a list of text segments along with their time. Generate timestamps for the main topics discussed in the video. 
-            You should format the timestamps as [hh:mm:ss](%VIDEO_URL?t=seconds).
+            Markdown for output:
+            1. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 1%
+            2. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 2%
+            - and so on
 
-            Ensure the topics are clear and concise, and aim for 5-7 main topics within the specified video transcript.(mention only topics titles and timestamps)
-            give the list of topics with time stamps
-            markdown for output 
-            "example :  1. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 1%
-                        2. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 2%
-                        -and so on" make time clickable
+            Markdown Example:
+            1. [00:05:23](https://youtu.be/hCaXor?t=323) Introduction
+            2. [00:10:45](https://youtu.be/hCaXor?t=645) Main Topic 1
+            3. [00:25:17](https://youtu.be/hCaXor?t=1517) Main Topic 2
+            - and so on
 
-            The %VIDEO_URL% (yotube video link) and transcript are given below.        
+            The %VIDEO_URL% (YouTube video link) and transcript are provided below :
             """
 
         else:
