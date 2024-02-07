8d4b01e818588249e47a0a6b5ad29073d3042149
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-07 20:05:40 +0530

    Added extra var for prompt

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ class Model():
     def __init__():
         pass
 
-    def google_gemini(transcript,prompt):
+    def google_gemini(transcript,prompt,extra=""):
         genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
         model = genai.GenerativeModel("gemini-pro")
-        response = model.generate_content(prompt+transcript)
+        response = model.generate_content(prompt+extra+transcript)
         return response.text
