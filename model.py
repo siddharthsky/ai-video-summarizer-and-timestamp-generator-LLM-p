dc1b6ae5bdea04d681493452ca81685e33a450de
Siddharth Kamble <siddharthsk7t@gmail.com>
2024-02-08 21:38:30 +0530

    Added extra var for prompt

--- model.py
+++ model.py
@@ -1,17 +1,15 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
-
 import google.generativeai as genai
 
+class Model:
+    def __init__(self):
+        load_dotenv()
 
-
-class Model():
-    def __init__():
-        pass
-
-    def google_gemini(transcript,prompt,extra=""):
+    @staticmethod
+    def google_gemini(transcript, prompt, extra=""):
+        load_dotenv()
         genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
         model = genai.GenerativeModel("gemini-pro")
-        response = model.generate_content(prompt+extra+transcript)
+        response = model.generate_content(prompt + extra + transcript)
         return response.text
