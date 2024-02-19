bf62502c126abad1dae01d00863299505cd5e0ea
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-02-19 22:32:08 +0530

    modified: model.py

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ import google.generativeai as genai
 
 class Model:
     def __init__(self):
-        load_dotenv()
+        load_dotenv() # Load environment variables from .env file
 
     @staticmethod
     def google_gemini(transcript, prompt, extra=""):
-        load_dotenv()
+        load_dotenv() 
         genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
         model = genai.GenerativeModel("gemini-pro")
         response = model.generate_content(prompt + extra + transcript)
