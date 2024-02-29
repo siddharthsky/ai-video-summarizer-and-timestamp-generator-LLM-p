f54b7727f0e2f2ad076fd5e96b2465e015d715f5
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-02-29 23:26:16 +0530

    Add load_env variables

--- pipeline/model.py
+++ pipeline/model.py
@@ -9,6 +9,7 @@ class Model:
 
     @staticmethod
     def google_gemini(transcript, prompt, extra=""):
+        load_dotenv()
         genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
         model = genai.GenerativeModel("gemini-pro")
         response = model.generate_content(prompt + extra + transcript)
@@ -16,6 +17,7 @@ class Model:
     
     @staticmethod
     def openai_chatgpt(transcript, prompt, extra=""):
+        load_dotenv()
         client =   OpenAI(api_key=os.getenv("OPENAI_CHATPGPT_API_KEY2"))
         model="gpt-3.5-turbo"
         message = [{"role": "system", "content": prompt + extra + transcript}]
