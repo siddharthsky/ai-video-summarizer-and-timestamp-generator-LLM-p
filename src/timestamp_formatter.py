525850c2160c4e53a746fa45bb1640279a197846
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-05-31 13:47:55 +0530

    Replaced re with strip-markdown

--- src/timestamp_formatter.py
+++ src/timestamp_formatter.py
@@ -1,17 +1,8 @@
-import re
+import strip_markdown
 
 class TimestampFormatter:
 
     @staticmethod
     def format(gentext):
-        pattern= r"(\d{2}:\d{2}:\d{2})\]\((?:https:\/\/youtu\.be\/[A-Za-z0-9_-]+\?t=\d+)\) ([^\n]+)"
-        finder = re.findall(pattern,gentext)
-        output = []
-        for i in finder:
-            print(f"{i[0]+' '+i[1]}")
-            output.append(f"{i[0]+' '+i[1]} \n")
-
-        cp_text = " ".join(output)
+        cp_text: str = strip_markdown.strip_markdown(gentext)
         return cp_text
-
-
