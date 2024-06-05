df030cae8054225c3cdb43e343735a91177950f6
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-06-05 22:16:08 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -28,7 +28,32 @@ Additional Tips:
 
 By following these guidelines, you can effectively transform video transcripts into captivating and informative summaries, drawing in readers and conveying the video's essence effectively."""
 
-        elif ID == "transcript":
+        elif ID == "timestamp":
+            prompt_text = """
+            Generate timestamps for main chapter/topics in a YouTube video transcript.
+            Given text segments with their time, generate timestamps for main topics discussed in the video. Format timestamps as hh:mm:ss and provide clear and concise topic titles.  
+           
+            Instructions:
+            1. List only topic titles and timestamps.
+            2. Do not explain the titles.
+            3. Include clickable URLs.
+            4. Provide output in Markdown format.
+
+            Markdown for output:
+            1. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 1%
+            2. [hh:mm:ss](%VIDEO_URL?t=seconds) %TOPIC TITLE 2%
+            - and so on
+
+            Markdown Example:
+            1. [00:05:23](https://youtu.be/hCaXor?t=323) Introduction
+            2. [00:10:45](https://youtu.be/hCaXor?t=645) Main Topic 1
+            3. [00:25:17](https://youtu.be/hCaXor?t=1517) Main Topic 2
+            - and so on
+
+            The %VIDEO_URL% (YouTube video link) and transcript are provided below :
+            """
+            
+         elif ID == "transcript":
             prompt_text = """
             Generate timestamps for main chapter/topics in a YouTube video transcript.
             Given text segments with their time, generate timestamps for main topics discussed in the video. Format timestamps as hh:mm:ss and provide clear and concise topic titles.  
